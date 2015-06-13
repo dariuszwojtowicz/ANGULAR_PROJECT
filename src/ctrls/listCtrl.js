e2f9475fa5dd8c3dceb90c1e7a7d085c79fa2d2a
ellheat <patryk.poleszczuk@gmail.com>
2015-06-13 16:13:32 +0200

    delete idea directory and add invalid filter mails

--- src/ctrls/listCtrl.js
+++ src/ctrls/listCtrl.js
@@ -21,17 +21,51 @@ app.controller('ListCtrl', function ($scope, $http) {
 			$scope.getEmails();
 			$scope.getSent();
 		});
-	}
+	};
 
 	$scope.read = function () {
 		var id = this.mail.id;
 		$http.put('/emails/' + id, { read: true }).success(function (res) {
 			$scope.getEmails();
 		});
-	}
+	};
+
+    $scope.filterFunction = function(element) {
+        return element.title.match(/^Ma/) ? true : false;
+    };
 
 	$scope.getSent();
 	$scope.getEmails();
 	//setInterval(function () {$scope.getEmails()}, 1000 * $scope.interval);
 	//setInterval(function () {$scope.getSent()}, 1000 * $scope.interval);
 });
+
+//app.filter('searchFor', function(){
+//
+//    // All filters must return a function. The first parameter
+//    // is the data that is to be filtered, and the second is an
+//    // argument that may be passed with a colon (searchFor:searchString)
+//
+//    return function(arr, searchString){
+//
+//        if(!searchString){
+//            return arr;
+//        }
+//
+//        var result = [];
+//
+//        searchString = searchString.toLowerCase();
+//
+//        // Using the forEach helper method to loop through the array
+//        angular.forEach(arr, function(item){
+//
+//            if(item.title.toLowerCase().indexOf(searchString) !== -1){
+//                result.push(item);
+//            }
+//
+//        });
+//
+//        return result;
+//    };
+//
+//});
