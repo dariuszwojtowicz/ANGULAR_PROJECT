79e6424f6ea5b65f2e903251716bff9f9fc74ccd
ellheat <patryk.poleszczuk@gmail.com>
2015-06-16 13:56:08 +0200

    add highlight in search result and change language to polish

--- src/ctrls/inboxCtrl.js
+++ src/ctrls/inboxCtrl.js
@@ -12,16 +12,25 @@ app.controller('InboxCtrl', function ($scope, $http, $location, emailService) {
 		emailService.deleteEmail(id).then(function(res) {
             $location.path("/inbox");
         });
-	}
+	};
 
 	$scope.read = function () {
 		var id = this.mail.id;
 		emailService.setAsRead(id).then(function(res) {
             $scope.getEmails();
         });
-	}
+	};
 
 	$scope.getEmails();
 
 	setInterval(function () { $scope.getEmails() }, 1000 * emailService.getRefreshInterval());
+});
+
+app.filter('highlight', function($sce) {
+    return function(text, phrase) {
+        if (phrase) text = text.replace(new RegExp('('+phrase+')', 'gi'),
+            '<span class="highlighted">$1</span>');
+
+        return $sce.trustAsHtml(text)
+    }
 });
